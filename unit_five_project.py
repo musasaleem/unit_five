9268790eaedfd84152b5efaf6c998bc9c0544af3
Musa <musa@iMac10.local>
2018-10-16 12:07:20 -0400

    finishing

--- unit_five_project.py
+++ unit_five_project.py
@@ -6,8 +6,6 @@ def game():
     if play in ["yes", "y", "yeah", "yup"]:
         print("Alright let's play the guessing game here's how you play:")
         print("I will choose a card between 1-100. I will give you hints if your guess is too low or too high.")
-        print("If you get it right in less than 5 tries, you win. "
-              "If you get my number within 5 or more tries, you lose.")
         return True
     else:
         return False
@@ -20,17 +18,23 @@ def draw():
 def main():
     while game():
         answer = draw()
+        guess_total = 0
         while True:
-            guess = input(int(("Guess my number"))
-            guess_total = guess + 1
+            guess = int(input("Guess my number"))
+            guess_total = guess_total + 1
             if guess > answer:
                 print("Your guess was too high")
+
             elif guess < answer:
                 print("Your guess was too low")
+
             elif guess == answer:
                 print("Congrats you got my number")
                 print("You got my number in", guess_total, "guesses.")
                 break
+    print("No games for you homie")
+
+
 
 
 main()
